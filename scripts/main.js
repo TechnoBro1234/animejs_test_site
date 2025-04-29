4740d5b8a999c0989b883b4ba6669a8b2e899d40
Victor DeMambro <awesomevictor2018@gmail.com>
2025-04-29 17:17:45 -0400

    feat: do a metric fuckton of stuff
    refactored out rotations variable, added sprite changing functionality, changed Ticsy from having class logo to having ID him and updated references accordingly

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,15 @@
 import { animate, createDraggable, createSpring, createScope } from "./libraries/anime.esm.js"; // YAY ESMODULES YAYYYYYYYYYYYYY
-let rotations = 0 // god i hate this
 
 function main() {
   function spin() {
-    rotations++
-    animate(".logo", {
-      rotate: rotations * 360,
+    animate("#him", {
+      rotate: "+=360",
       duration: 1500,
       ease: 'inOutQuint',
     });
   }
   function bounce() {
-    animate(".logo", {
+    animate("#him", {
       scale: [
         { to: 1.30, ease: 'inOutCirc', duration: 250 },
         { to: 1, ease: createSpring({ stiffness: 350 }) }
@@ -21,15 +19,20 @@ function main() {
   }
   function move() {
     console.log($("input#move_slider").val())
-    animate(".logo", {
+    animate("#him", {
       translateX: ($("input#move_slider").val()/100)*(window.innerWidth-140),
       duration: 1000,
       ease: createSpring({stifness: 300})
     })
   }
+  function change_sprite() {
+    console.log(`images/ticsy/${$("#sprite_dropdown").val()}.png`)
+    $("#him").attr("src", `images/ticsy/${$("#sprite_dropdown").val()}.png`)
+  }
   $("button#spin_button").click(spin)
   $("button#bounce_button").click(bounce)
   $("button#move_button").click(move)
+  $("#sprite_change_button").click(change_sprite)
 }
 
 
